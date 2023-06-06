d86215caf2b99563469c9366d6ac411de6688911
eduardokaique99 <eduardo_kaique99@hotmail.com>
2023-06-06 00:11:28 -0300

    06/05

---
+++ Estacionamento/Model/Estacionamento.cs
@@ -0,0 +1,90 @@
+ namespace Model
+  {
+  public class Estacionamento
+    {
+        public int Id { get; set; }
+        public int QtdVagas { get; set; }
+
+
+        public Estacionamento(int qtdVagas)
+        {
+            QtdVagas = qtdVagas;
+        }
+
+        public Estacionamento()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Codigo: {QtdVagas}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Estacionamento estacionamento = (Estacionamento)obj;
+            return Id == estacionamento.Id;
+        }
+
+        public static Model.Estacionamento CriarEstacionamento(
+            int qtdVagas
+        ) {
+            return new Model.Estacionamento(
+                qtdVagas
+            );
+        }
+        public static Model.Estacionamento AlterarEstacionamento(
+            int id,
+            int qtdVagas
+        )
+        {
+            Estacionamento estacionamento = BuscarEstacionamento(
+                id
+            );
+
+            estacionamento.QtdVagas = qtdVagas;
+
+            Repository.Context context = new Repository.Context();
+            context.Estacionamentos.Update(estacionamento);
+            context.SaveChanges();
+
+            return estacionamento;
+        }
+
+        public static Estacionamento ExcluirEstacionamento(
+            int id
+        )
+        {
+            Estacionamento estacionamento = BuscarEstacionamento(
+                id
+            );
+
+            Repository.Context context = new Repository.Context();
+            context.Estacionamentos.Remove(estacionamento);
+            context.SaveChanges();
+
+            return estacionamento;
+        }
+
+        public static Estacionamento BuscarEstacionamento(
+            int id
+        )
+        {
+            Repository.Context context = new Repository.Context();
+            return (
+                from a in context.Estacionamentos
+                where a.Id == id
+                select a
+            ).First();
+        }
+
+        public static IEnumerable<Estacionamento> BuscarTodos()
+        {
+            Repository.Context context = new Repository.Context();
+            return (
+                from a in context.Estacionamentos
+                select a
+            );
+        }
+    }
+}
---
+++ Estacionamento/Model/Veiculo.cs
@@ -0,0 +1,107 @@
+namespace Model
+  {
+  public class Veiculo
+    {
+        public int Id {get ; set;}
+        public string Placa {get; set;}
+        public int IdMovimentacao {get; set;}
+        public int IdTipo {get; set;}
+        public int IdCartao {get; set;}
+
+        public Veiculo(string Placa, int IdMovimentacao, int IdTipo, int IdCartao)
+        {
+            Placa = placa;
+            IdMovimentacao = idmovimentacao;
+            IdTipo = idtipo;
+            IdCartao = idcartao;
+        }
+
+        public Veiculo()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Placa: {placa}, Id da Movimentação: {IdMovimentacao}, Id do Tipo: {IdTipo}, Id do Cartão: {IdCartao}";
+        }
+
+        public override bool Equals (object obj)
+        {
+            Veiculo veiculo = (Veiculo)obj;
+            return Id == veiculo.Id;
+        }
+
+        public static Model.Veiculo CriarVeiculo(
+            int Id,
+            string Placa,
+            int IdMovimentacao,
+            int IdTipo,
+            int IdCartao
+        ){
+            return new Model.Veiculo(
+                id,
+                placa,
+                idmovimentacao,
+                idtipo,
+                idcartao
+            );
+        }
+
+        public static Model.Veiculo AlterarVeiculo(
+            int Id,
+            string Placa,
+            int IdMovimentacao,
+            int IdTipo,
+            int IdCartao
+        )
+        {
+            Veiculo veiculo = BuscarVeiculo(
+                id
+            );
+
+            veiculo.Placa = placa;
+
+            Repository.Context context = new Repository.Context();
+            context.Veiculos.Update(veiculo);
+            contexto.SaveChanges();
+
+            return veiculo;
+        }
+
+        public static Veiculo ExcluirVeiculo(
+            int id
+        )
+        {
+            Veiculo veiculo = BuscarVeiculo(
+                id
+            );
+
+            Repository.Context context = new Repository.Context();
+            context.Veiculos.Remove(veiculo);
+            context.SaveChanges();
+
+            return veiculo;
+        }
+
+        public static Veiculo BuscarVeiculo(
+            int id
+        )
+        {
+            Repository.Context context = new Repository.Context();
+            return (
+                from a in context.Veiculos
+                where a.Id == id
+                select a
+            ).First();
+        }
+
+        public static IEnumerable<Veiculo> BuscarTodos()
+        {
+            Repository.Context context = new Repository.Context();
+            return (
+                from a in context.Veiculos
+                select a
+            );
+        } 
+    }
+}
